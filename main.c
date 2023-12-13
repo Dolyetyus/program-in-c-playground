31665ea0657efffa504f3fed47d1a3fb445c5d53
Doli <67073431+Dolyetyus@users.noreply.github.com>
2023-12-13 17:05:06 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 
 int main() {
     int n, m;
+    int x;
     
     printf("Enter the number of rows (n): ");
     scanf("%d", &n);
@@ -23,17 +24,39 @@ int main() {
         printf("\n");
     }
 
-    while (1) {
-        int x;
-        
+    while (x >= 0) {
         printf("Number of people in the group: ");
         scanf("%d", &x);
-
         if (x < 0) {
             break;
         }
         
         printf("X=%d \n", x);
+        
+        int seek_row = 0;
+        int last_zero_id = -1;
+        
+        for (int j = m - 1; j >= 0; j--) {
+            if (arr[seek_row][j] == 0) {
+                last_zero_id = j;
+                break;
+            }
+        }
+        
+        int count = 0;
+        for (int j = last_zero_id; j >= 0 && count < x; j--) {
+            arr[seek_row][j] = 1;
+            count++;
+        }
+        
+        printf("Updated array:\n");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                printf("%d ", arr[i][j]);
+            }
+            printf("\n");
+        }
+        
     }
 
     return 0;
