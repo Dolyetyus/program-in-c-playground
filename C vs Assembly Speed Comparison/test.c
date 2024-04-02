eb03b040487b3517e3ef2eb236f98ddc41fe6d01
Doli <67073431+Dolyetyus@users.noreply.github.com>
2024-04-03 01:17:37 +0200

    Create test.c

---
+++ C vs Assembly Speed Comparison/test.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+    int num1 = 10, num2 = 5;
+    int add, subtract, multiply;
+    float divide;
+
+    time_t start, end;
+    double cpu_time_used;
+
+    start = clock();
+    for (int i = 0; i < 1000000000; i++){
+        add = num1 + num2;
+        subtract = num1 - num2;
+        multiply = num1 * num2;
+        divide = num1 / (float)num2;
+    }
+    end = clock();
+
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+    printf("Time taken to perform operations: %f seconds\n", cpu_time_used);
+
+    return 0;
+}
