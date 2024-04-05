79a75324087506c136322c8a8eb8fd2dae375096
Doli <67073431+Dolyetyus@users.noreply.github.com>
2024-04-05 02:21:41 +0200

    Update quicksort.c

--- quicksort/quicksort.c
+++ quicksort/quicksort.c
@@ -5,7 +5,7 @@ int partition(int arr[], int low, int high) {
     int pivot = arr[high];
     int i = (low - 1);
 
-    for (int j = low; j <= high - 1; j++) {
+    for (int j = low; j < high; j++) {
         if (arr[j] < pivot) {
             i++;
             int temp = arr[i];
